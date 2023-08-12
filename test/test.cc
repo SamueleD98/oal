a574a9dfb0f3e618bc3b26b94cc4112fe09ea46b
SamueleD98 <depalo.samuele@gmail.com>
2023-08-12 20:18:32 +0200

    Update test.cc

--- test/test.cc
+++ test/test.cc
@@ -1,17 +1,16 @@
 
 #include "oal/path_planner.hpp"
 
-int main(int, char**)
-{
+int main(int, char **) {
   VehicleInfo v_info;
-  Eigen::Vector2d position = {10,0};
+  Eigen::Vector2d position = {10, 0};
   v_info.position = position;
   v_info.speed = 2;
 
   // Obstacle constructor arguments: string id, Eigen::Vector2d position, double heading, double speed, double dim_x, double dim_y, double max_bb_ratio, double safety_bb_ratio
-  Obstacle obs1 = Obstacle("1", {9,3}, 0, 0.9, 0.5, 0.5, 2, 1.6 );
-  Obstacle obs2 = Obstacle("2", {10, 4.1}, 3.14, 0, 0.4, 0.4, 2, 1.6 );
-  Obstacle obs3 = Obstacle("3", {11, 0.5}, 3.14*3/4, 1.8, 0.2, 0.2, 2, 1.6 );
+  Obstacle obs1 = Obstacle("1", {10, 3}, 3.14 / 2, 0.8, 0.5, 0.5, 2, 1.6);
+  Obstacle obs2 = Obstacle("2", {10, 4.09}, 3.14, 0.5, 0.4, 0.4, 2, 1.6);
+  Obstacle obs3 = Obstacle("3", {11, 0.5}, 3.14 * 3 / 4, 1.8, 0.2, 0.2, 2, 1.6);
 
   ObstaclesInfo obss_info;
   obss_info.obstacles.push_back(obs1);
@@ -20,10 +19,10 @@ int main(int, char**)
 
   path_planner planner(v_info, obss_info);
 
-  Eigen::Vector2d goal = {10,5};
+  Eigen::Vector2d goal = {10, 5};
   std::stack<Node> waypoints;
   std::cout << "Calling library!" << std::endl;
-  if(planner.ComputePath(goal, waypoints)){
+  if (planner.ComputePath(goal, waypoints)) {
     //std::cout << "Found!" << std::endl;
     std::cout << "Found!" << std::endl;
     while (!waypoints.empty()) {
@@ -32,7 +31,7 @@ int main(int, char**)
       //std::cout << "Pos: " << wp.position << std::endl;
       //std::cout << "Time: " << wp.time << std::endl;
     }
-  }else{
+  } else {
     std::cout << "Not found." << std::endl;
   }
 }
