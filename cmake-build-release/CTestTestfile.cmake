04366e5ad412d4f350b46fa6677d6dfd1299c164
samuele <depalo.samuele@gmail.com>
2023-11-21 14:30:38 +0100

    removed useless stuff

--- cmake-build-release/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/samuele/graal_ws/oal
-# Build directory: /home/samuele/graal_ws/oal/cmake-build-release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(oal_test "oal_test")
-set_tests_properties(oal_test PROPERTIES  _BACKTRACE_TRIPLES "/home/samuele/graal_ws/oal/CMakeLists.txt;81;add_test;/home/samuele/graal_ws/oal/CMakeLists.txt;0;")
