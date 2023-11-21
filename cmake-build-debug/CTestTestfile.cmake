f379ebeeb5b091da2dcb60f42fb877cad76c0929
samuele <depalo.samuele@gmail.com>
2023-11-21 14:53:17 +0100

    Revert "removed useless stuff"
    This reverts commit 04366e5ad412d4f350b46fa6677d6dfd1299c164.

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/samuele/graal_ws/oal
+# Build directory: /home/samuele/graal_ws/oal/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(oal_test "oal_test")
+set_tests_properties(oal_test PROPERTIES  _BACKTRACE_TRIPLES "/home/samuele/graal_ws/oal/CMakeLists.txt;81;add_test;/home/samuele/graal_ws/oal/CMakeLists.txt;0;")
---
+++ cmake-build-release/CMakeFiles/oal.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/samuele/graal_ws/oal/src/helper_functions.cpp" "CMakeFiles/oal.dir/src/helper_functions.cpp.o" "gcc" "CMakeFiles/oal.dir/src/helper_functions.cpp.o.d"
+  "/home/samuele/graal_ws/oal/src/node.cpp" "CMakeFiles/oal.dir/src/node.cpp.o" "gcc" "CMakeFiles/oal.dir/src/node.cpp.o.d"
+  "/home/samuele/graal_ws/oal/src/obstacle.cpp" "CMakeFiles/oal.dir/src/obstacle.cpp.o" "gcc" "CMakeFiles/oal.dir/src/obstacle.cpp.o.d"
+  "/home/samuele/graal_ws/oal/src/path_planner.cpp" "CMakeFiles/oal.dir/src/path_planner.cpp.o" "gcc" "CMakeFiles/oal.dir/src/path_planner.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
